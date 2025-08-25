be26912a227335494515fd9eaa21590043564610
mwisniewski1991 <mw.data.web@gmail.com>
2025-08-25 22:43:15 +0200

    Add docker configuration

---
+++ app/website/gunicorn.conf.py
@@ -0,0 +1,14 @@
+# Gunicorn configuration file
+bind = "0.0.0.0:34576"
+workers = 4
+worker_class = "sync"
+worker_connections = 1000
+max_requests = 1000
+max_requests_jitter = 100
+timeout = 30
+keepalive = 2
+user = None
+group = None
+tmp_upload_dir = None
+logconfig = None
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' 
